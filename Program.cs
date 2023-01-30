12053a1f04019982b0f453596178412491f76451
isabelle galvão <isabellegalvao123@gmail.com>
2023-01-30 15:31:14 -0300

    construction Menu

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace TextEditor
     static void Main(string[] args)
     {
 
+      Menu();
     }
 
     static void Menu()
@@ -17,7 +18,27 @@ namespace TextEditor
       Console.WriteLine("(1) - Abrir arquivo");
       Console.WriteLine("(2) - Criar um novo arquivo");
       Console.WriteLine("(0) - Sair");
+      short option = short.Parse(Console.ReadLine());
+
+      switch (option)
+      {
+        case 0: System.Environment.Exit(0); break;
+        case 1: Open(); break;
+        case 2: Editor(); break;
+        default: Menu(); break;
+
+      }
+    }
+    static void Open()
+    {
 
     }
+
+    static void Editor()
+    {
+
+    }
+
+
   }
 }
