2402e66193ecb02e4536ee477526e2b71568d83a
isabelle galvão <isabellegalvao123@gmail.com>
2023-01-31 13:33:36 -0300

    Adding opening function

--- Program.cs
+++ Program.cs
@@ -32,7 +32,18 @@ namespace TextEditor
     }
     static void Open()
     {
+      Console.Clear();
+      Console.WriteLine("Qual o caminho do arquivo que deseja abrir?");
+      string path = Console.ReadLine();
+      using (var file = new StreamReader(path))
+      {
+        string text = file.ReadToEnd();
+        Console.WriteLine(text);
+      }
 
+      Console.WriteLine("");
+      Console.ReadLine();
+      Menu();
     }
 
     static void Editor()
